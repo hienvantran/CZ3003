8e898f169a802d8761f620339edda5b8301b97b0
Mark <mtrhorange@gmail.com>
2022-09-09 21:53:36 +0800

    FirebaseManager.cs commented out
    Commented out FirebaseManager.cs due to compile errors from not having firebase package.
    To be uncommented after importing package properly.

--- Assets/Scripts/FirebaseManager.cs
+++ Assets/Scripts/FirebaseManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Firebase;
@@ -243,3 +243,4 @@ public class FirebaseManager : MonoBehaviour
         }
     }
 }
+*/
