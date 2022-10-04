aa7acc0bc4543433850161ab21f03231f3b8832a
Benjamin <gohbwj@gmail.com>
2022-10-04 19:58:36 +0800

    Game End and Level progression / unlocking

--- Assets/Scripts/Runnergame/LevelManager.cs
+++ Assets/Scripts/Runnergame/LevelManager.cs
@@ -12,6 +12,9 @@ public class LevelManager : MonoBehaviour
     public QuestionManager.OpMode runnerOpMode = QuestionManager.OpMode.ADD;
     public string customQuestions;
 
+    public int addProgress = 0, subProgress = 0, mulProgress = 0, divProgress = 0;
+    public string currentLevel = string.Empty;
+
     private void Awake()
     {
         if (!LevelManager.instance)
@@ -19,6 +22,8 @@ public class LevelManager : MonoBehaviour
 
         if (LevelManager.instance != this)
             Destroy(this.gameObject);
+
+        GetUserProgress();
 
         DontDestroyOnLoad(this.gameObject);
     }
@@ -37,5 +42,59 @@ public class LevelManager : MonoBehaviour
         customQuestions = cusQuestions;
     }
 
+    //set current level
+    public void SetLevel(string level)
+    {
+        currentLevel = level;
+    }
 
+    //Gets the user's progress of the normal levels
+    public void GetUserProgress()
+    {
+        //to edit: maybe check firebase auth confirm user login
+
+        //to edit: pull firestore user progress fields
+        addProgress = 0;
+        subProgress = 0;
+        mulProgress = 2;
+        divProgress = 1;
+    }
+
+    //Updates the user's progress of the normal levels
+    public void UpdateUserProgress(int score, bool unlockNext)
+    {
+        //to edit: maybe check firebase auth confirm user login
+
+        //update the level progress
+        if (unlockNext)
+        {
+            switch (currentLevel[0])
+            {
+                case 'A':
+                    addProgress = (int)char.GetNumericValue(currentLevel[1]);
+                    //to edit: push firestore user progress fields
+                    //push
+                    break;
+                case 'S':
+                    subProgress = (int)char.GetNumericValue(currentLevel[1]);
+                    //to edit: push firestore user progress fields
+                    //push
+                    break;
+                case 'M':
+                    mulProgress = (int)char.GetNumericValue(currentLevel[1]);
+                    //to edit: push firestore user progress fields
+                    //push
+                    break;
+                case 'D':
+                    divProgress = (int)char.GetNumericValue(currentLevel[1]);
+                    //to edit: push firestore user progress fields
+                    //push
+                    break;
+            }
+        }
+
+        //to edit: then also push to firestore the completion records to Score collection
+        //push score, user id, etc..
+
+    }
 }
--- Assets/Scripts/Runnergame/MenuButton.cs
+++ Assets/Scripts/Runnergame/MenuButton.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
@@ -10,14 +11,15 @@ public class MenuButton : MonoBehaviour
     public int numQns;
     public float difficulty;
     public QuestionManager.OpMode opMode;
-    public string seed;
+    public string levelString;
 
     [SerializeField] private TMP_InputField seedInput;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        CheckProgress();
     }
 
     // Update is called once per frame
@@ -26,10 +28,11 @@ public class MenuButton : MonoBehaviour
         
     }
 
-    //load normal
+    //Load normal
     public void LoadLevel()
     {
         LevelManager.instance.SetParams(numQns, difficulty, opMode);
+        LevelManager.instance.SetLevel(levelString);
         SceneManager.LoadScene("RunnerGame");
     }
 
@@ -45,4 +48,72 @@ public class MenuButton : MonoBehaviour
         SceneManager.LoadScene("RunnerGame");
     }
 
+    //tooltip for levels that are still locked
+    public void MouseEnter()
+    {
+        if (!GetComponent<Button>().interactable)
+        {
+            transform.Find("Tooltip").gameObject.SetActive(true);
+        }
+    }
+    public void MouseExit()
+    {
+        if (!GetComponent<Button>().interactable)
+        {
+            transform.Find("Tooltip").gameObject.SetActive(false);
+        }
+    }
+
+    //check progress and set interactability
+    private void CheckProgress()
+    {
+        //Add
+        if (opMode == QuestionManager.OpMode.ADD)
+        {
+            if (this.gameObject.name.Equals("Level2Btn") && LevelManager.instance.addProgress >= 1)
+            {
+                GetComponent<Button>().interactable = true;
+            }
+            else if (this.gameObject.name.Equals("Level3Btn") && LevelManager.instance.addProgress >= 2)
+            {
+                GetComponent<Button>().interactable = true;
+            }
+        }
+        //Sub
+        else if (opMode == QuestionManager.OpMode.SUB)
+        {
+            if (this.gameObject.name.Equals("Level2Btn") && LevelManager.instance.subProgress >= 1)
+            {
+                GetComponent<Button>().interactable = true;
+            }
+            else if (this.gameObject.name.Equals("Level3Btn") && LevelManager.instance.subProgress >= 2)
+            {
+                GetComponent<Button>().interactable = true;
+            }
+        }
+        //Mul
+        else if (opMode == QuestionManager.OpMode.MUL)
+        {
+            if (this.gameObject.name.Equals("Level2Btn") && LevelManager.instance.mulProgress >= 1)
+            {
+                GetComponent<Button>().interactable = true;
+            }
+            else if (this.gameObject.name.Equals("Level3Btn") && LevelManager.instance.mulProgress >= 2)
+            {
+                GetComponent<Button>().interactable = true;
+            }
+        }
+        //Div
+        else if (opMode == QuestionManager.OpMode.DIV)
+        {
+            if (this.gameObject.name.Equals("Level2Btn") && LevelManager.instance.divProgress >= 1)
+            {
+                GetComponent<Button>().interactable = true;
+            }
+            else if (this.gameObject.name.Equals("Level3Btn") && LevelManager.instance.divProgress >= 2)
+            {
+                GetComponent<Button>().interactable = true;
+            }
+        }
+    }
 }
